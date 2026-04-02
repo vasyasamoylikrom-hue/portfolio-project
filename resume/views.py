f156f93fa67400d69f9fd305f5461fc3184872c8
Samoylik <vasya.samoylik.rom@gmail.com>
2026-04-02 22:00:56 +0300

    Views added

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Profile, Education, Skill
 
-# Create your views here.
+def resume_view(request):
+    profile = get_object_or_404(Profile, pk=1)
+    context = {
+        'profile': profile,
+        'experiences': profile.experiences.all(),
+        'education': profile.education.all(),
+        'skills': profile.skills.all(),
+    }
+    return render(request, 'resume/resume.html', context)
--- resume_project/settings.py
+++ resume_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'resume',
-    'rest_framework',
+    #'rest_framework',
 ]
 
 MIDDLEWARE = [
